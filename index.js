42ead6d49144e22dc8e4da6246dc006acc269a53
Joanne Ong <joanneongcf@u.nus.edu>
2025-12-13 23:24:15 +0800

    Add logic for model prediction

--- index.js
+++ index.js
@@ -12,36 +12,47 @@ window.addEventListener('load', () => {
 const imgInput = document.getElementById('imgInput');
 const imgPreview = document.getElementById('imgPreview');
 const previewText = document.getElementById('previewText');
+const tasteButton = document.getElementById('tasteButton');
+const verdict = document.getElementById('verdict');
 
 imgInput.addEventListener('change', event => {
     const file = event.target.files[0];
+    if (!file) {
+        imgPreview.src = '#';
+        imgPreview.style.display = 'none';
+        previewText.style.display = 'block';
+        tasteButton.style.display = 'none';
+        return;
+    }
+
     const imgUrl = URL.createObjectURL(file);
     previewText.style.display = 'none';
     imgPreview.src = imgUrl;
     imgPreview.style.display = 'block';
 });
 
-// const cat = document.getElementById('cat');
-// cat.onload = async () => {
-//   console.log("Loaded cat!")
-//   const resultElement = document.getElementById('result');
+imgPreview.onload = async() => {
+    console.log('Image is loaded');
+    tasteButton.style.display = 'block';
+}
 
-//   resultElement.innerText = 'Loading MobileNet...';
+tasteButton.addEventListener('click', event => {
+    event.preventDefault();
+    tasteButton.disabled = true;
 
-//   const pixels = tf.browser.fromPixels(cat);
+    const pixels = tf.browser.fromPixels(imgPreview);
+    console.time('Prediction');
+    let result = mobileNet.predict(pixels);
+    const topK = mobileNet.getTopKClasses(result, 1);
+    console.timeEnd('Prediction');
 
-//   console.time('Prediction');
-//   let result = mobileNet.predict(pixels);
-//   const topK = mobileNet.getTopKClasses(result, 5);
-//   console.timeEnd('Prediction');
+    verdict.innerText = '';
+    topK.forEach(x => {
+        verdict.innerText += `${x.value.toFixed(3)}: ${x.label}\n`;
+    });
+    tasteButton.disabled = false;
+});
 
-//   resultElement.innerText = '';
-//   topK.forEach(x => {
-//     resultElement.innerText += `${x.value.toFixed(3)}: ${x.label}\n`;
-//   });
-
-//   mobileNet.dispose();
-// };
-// cat.src='./cat.jpg'
-
-
+window.addEventListener('beforeunload', () => {
+    mobileNet.dispose();
+});
