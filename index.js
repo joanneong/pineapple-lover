cf00bf1d9d7b8b94a81a6cc068f173fc8fba00df
Joanne Ong <joanneongcf@u.nus.edu>
2025-12-13 23:55:42 +0800

    Add verdict panel

--- index.js
+++ index.js
@@ -14,14 +14,21 @@ const imgPreview = document.getElementById('imgPreview');
 const previewText = document.getElementById('previewText');
 const tasteButton = document.getElementById('tasteButton');
 const verdict = document.getElementById('verdict');
+const verdictText = document.getElementById('verdictText');
+const topKText = document.getElementById('topKText');
+
+function resetElements() {
+    imgPreview.src = '#';
+    imgPreview.style.display = 'none';
+    previewText.style.display = 'block';
+    tasteButton.style.display = 'none';
+    verdict.style.display = 'none';
+}
 
 imgInput.addEventListener('change', event => {
     const file = event.target.files[0];
     if (!file) {
-        imgPreview.src = '#';
-        imgPreview.style.display = 'none';
-        previewText.style.display = 'block';
-        tasteButton.style.display = 'none';
+        resetElements();
         return;
     }
 
@@ -29,28 +36,41 @@ imgInput.addEventListener('change', event => {
     previewText.style.display = 'none';
     imgPreview.src = imgUrl;
     imgPreview.style.display = 'block';
+    verdict.style.display = 'none';
 });
 
 imgPreview.onload = async() => {
-    console.log('Image is loaded');
     tasteButton.style.display = 'block';
+}
+
+function getVerdict(prediction) {
+    if (!prediction.label.includes('pineapple')) {
+        return "This is NOT a pineapple :(";
+    }
+
+    if (prediction.value < 0.8) {
+        return "This may be a pineapple...or is it?";
+    }
+
+    return "This is most likely a pineapple :)";
 }
 
 tasteButton.addEventListener('click', event => {
     event.preventDefault();
-    tasteButton.disabled = true;
+    tasteButton.style.display = 'none';
 
     const pixels = tf.browser.fromPixels(imgPreview);
     console.time('Prediction');
     let result = mobileNet.predict(pixels);
-    const topK = mobileNet.getTopKClasses(result, 1);
+    const topK = mobileNet.getTopKClasses(result, 3);
     console.timeEnd('Prediction');
 
-    verdict.innerText = '';
+    verdictText.innerText = getVerdict(topK[0]);
+    topKText.innerHTML = '';
     topK.forEach(x => {
-        verdict.innerText += `${x.value.toFixed(3)}: ${x.label}\n`;
+        topKText.innerHTML += `${x.value.toFixed(3)}: ${x.label}<br>`;
     });
-    tasteButton.disabled = false;
+    verdict.style.display = 'block';
 });
 
 window.addEventListener('beforeunload', () => {
